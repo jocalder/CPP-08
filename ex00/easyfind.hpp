e6be1d948bab075acaab26bee0dc516dcbdacbe4
jocalder <jonathancalderonbonilla24@gmail.com>
2026-04-08 11:18:56 +0200

    done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jocalder <jocalder@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 09:03:26 by jocalder          #+#    #+#             */
-/*   Updated: 2026/04/08 09:47:06 by jocalder         ###   ########.fr       */
+/*   Updated: 2026/04/08 11:14:44 by jocalder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,27 @@
 #include <vector>
 #include <list>
 #include <deque>
-//#include <algorithm>
+#include <algorithm>
 #include <exception>
 
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
-	typename T::iterator	it = container.begin();
-	while (it != container.end())
-	{
-		if (*it == value)
-			return it;
-		++it;
-	}	
-	throw std::runtime_error("Value not found in container");
+	typename T::iterator	it =
+		std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found in container");
+	return it;
 }
 
 template <typename T>
 typename T::const_iterator easyfind(const T& container, int value)
 {
-	typename T::const_iterator	it = container.begin();
-	while (it != container.end())
-	{
-		if (*it == value)
-			return it;
-		++it;
-	}	
-	throw std::runtime_error("Value not found in container");
+	typename T::const_iterator	it =
+		std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found in container");
+	return it;
 }
 
 #endif
